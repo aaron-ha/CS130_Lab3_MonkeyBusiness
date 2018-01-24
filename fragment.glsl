de4aecb0804ea8b96a38e0bf336519bd7f4a8dc0
Aaron Ha <ha_aaron@outlook.com>
2018-01-24 13:00:20 -0800

    finished step 6

--- fragment.glsl
+++ fragment.glsl
@@ -1,8 +1,7 @@
-varying vec3 pos;
-void main() {
-    vec3 eye=normalize(-pos);
-    if(eye.y < 0)
- 	gl_FragColor = vec4(1,0,0,1)*abs(eye.y); 
-    else
-	gl_FragColor = vec4(0,0,1,1)*eye.y;
+varying vec3 lightDir;
+void main()
+{
+    vec3 l=normalize(lightDir);
+    gl_FragColor=vec4(abs(l),1);    
 }
+         
--- vertex.glsl
+++ vertex.glsl
@@ -1,7 +1,9 @@
+varying vec3 lightDir;
 varying vec3 pos;
-void main() {
-  gl_Position = gl_ProjectionMatrix 
-      * gl_ModelViewMatrix
-      * gl_Vertex;
-  pos = (gl_ModelViewMatrix * gl_Vertex).xyz;
+void main()
+{
+    pos=(gl_ModelViewMatrix * gl_Vertex).xyz;
+    gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;
+    lightDir=normalize(gl_LightSource[0].position.xyz-pos);
 }
+    
